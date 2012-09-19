586055b9737b8ecaffacf346b9d241b359b0cf0a
Valtteri Virtanen <valtteri@taiste.fi>
2012-09-19 17:37:32 +0300

    Cleaned up the bootstrap and added very ugly circle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(function () {
+    var canvas = $('#main-canvas')[0].getContext("2d");
 
+    canvas.beginPath();
+    canvas.arc(140, 80, 50, 0, Math.PI*2, true); 
+    canvas.closePath();
+    canvas.stroke();
+});
