fa0006a3ec745c54694e71987b75c0770004fe71
Roope Merikukka <roope.merikukka@gmail.com>
2012-10-02 14:12:28 +0300

    päivitetty audiomahdollisuus preloaderiin

--- js/preloader.js
+++ js/preloader.js
@@ -4,11 +4,24 @@ function preloadImage(arrayOfImages) {
 	});
 }
 
+function preloadAudio(arrayOfAudio) {
+	$(arrayOfAudio).each(function() {
+		var audio = document.createElement("audio");
+		audio.src = this;
+	});
+}
+
 /* Usage
 preloadImage([
 	'kuva1.jpg',
 	'kuva2.jpg',
 	'kuva3.jpg'
+]);
+
+preloadAudio([
+	'audio1.mp3',
+	'audio2.mp3',
+	'audio3.mp3'
 ]);
 
 */
