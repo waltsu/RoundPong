8b350f12257367a5440f3fb03aeef182a01add86
Roope Merikukka <roope.merikukka@gmail.com>
2012-10-31 14:37:46 +0200

    configuring sql query

--- commit.php
+++ commit.php
@@ -13,7 +13,7 @@ $time = $_POST['time'];
 try {
     $db = new PDO('mysql:host=localhost;dbname=roundpong', $user, $pass);
     echo '1';
-    $db->exec("insert into score (nick, time, score) values ($nick, $time, $score)");
+    $db->exec("insert into score (nick, time, score) values ('kissa', 'aika', 0)");
     echo '2';
     $db = null;
 } catch (PDOException $e) {
