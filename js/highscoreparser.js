46e44aae4922835dbb81685d65a5c415fbf7dbec
Roope Merikukka <roope.merikukka@gmail.com>
2012-11-01 13:34:14 +0200

    fixing parsers functioning

--- js/highscoreparser.js
+++ js/highscoreparser.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
   $.getJSON("commit.php",function(result){
-    $.each(result, function(i, field){
-      $(".score-board").append(field + " ");
-    });
+    console.log(result);
   });
 });
