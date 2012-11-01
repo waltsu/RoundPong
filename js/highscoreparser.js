72e87873bfbc78705805aebb3aaaebb8ea8322ef
Roope Merikukka <roope.merikukka@gmail.com>
2012-11-01 14:35:10 +0200

    fixing parsers functioning

--- js/highscoreparser.js
+++ js/highscoreparser.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $.getJSON("commit.php",function(result){
     $.each(result, function(i){
-      console.log('nick: ' + result[i].nick);
+      console.log(result[i]);
       });
     });
   });
