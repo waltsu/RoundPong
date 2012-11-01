88a3e3ee520ed5003a3837b9421073e724eb777d
Roope Merikukka <roope.merikukka@gmail.com>
2012-11-01 13:20:30 +0200

    fixing typos

--- js/highscoreparser.js
+++ js/highscoreparser.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.getJSON("../commit.php",function(result){
+  $.getJSON("commit.php",function(result){
     $.each(result, function(i, field){
       $(".score-board").append(field + " ");
     });
