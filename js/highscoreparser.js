beac594bc612caacb6149eb1634e416e677529e7
Roope Merikukka <roope.merikukka@gmail.com>
2012-11-06 20:50:52 +0200

    added interval to load highscores

--- js/highscoreparser.js
+++ js/highscoreparser.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-  $.getJSON("commit.php",function(result){
-    $.each(result, function(i){
-      $('.score-board table').append('<tr><td>' + result[i].nick + '</td><td>' + result[i].time + '</td><td>' + result[i].score + '</td></tr>');
-    });
-  });
+
+	function loadHighscore(){
+		$.getJSON("commit.php",function(result){
+    		$.each(result, function(i){
+      			$('.score-board table').append('<tr><td>' + result[i].nick + '</td><td>' + result[i].time + '</td><td>' + result[i].score + '</td></tr>');
+    		});
+  		});
+	}
+
+	setInterval(loadHighscore, 30000);
+
+	loadHighscore();
 });
