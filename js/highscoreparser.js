c93c9dd9ef957f8aa272b3624383eb9f0d2e03f4
Roope Merikukka <roope.merikukka@gmail.com>
2012-11-07 11:03:05 +0200

    fixed problem with tableheaders disappearing

--- js/highscoreparser.js
+++ js/highscoreparser.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	function loadHighscore(){
 		$.getJSON("commit.php",function(result){
 			$('.score-board table').empty();
+			$('.score-board table').append('<tr><th>Nickname</th><th>Time played</th><th>Score</th></tr>');
     		$.each(result, function(i){
       			$('.score-board table').append('<tr><td>' + result[i].nick + '</td><td>' + result[i].time + '</td><td>' + result[i].score + '</td></tr>');
     		});
