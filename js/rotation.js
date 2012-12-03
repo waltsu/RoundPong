80aa0ff441e3da99df1d41a5c7745062e581ffb5
Roope Merikukka <roope.merikukka@gmail.com>
2012-12-03 16:15:35 +0200

    fixed typo

--- js/rotation.js
+++ js/rotation.js
@@ -1,14 +1,13 @@
 var value = 0;
-$(".refresh-image").rotate({ 
-   bind: 
-     { 
-        click: function(){
-            value +=720;
-            $(this).rotate({
-                animateTo:value,
-                duration:2000
-            });
-        }
-     } 
-   
+$(".refresh-image").rotate({
+	bind: 
+	{
+		click: function(){
+			value +=720;
+			$(this).rotate({
+				animateTo:value,
+				duration:2000
+			});
+		}
+	}
 });​
