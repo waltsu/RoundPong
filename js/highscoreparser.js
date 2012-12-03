32cdfe91b9d1b415c604e690c4c91d5ee5fe2134
Roope Merikukka <roope.merikukka@gmail.com>
2012-12-03 10:39:02 +0200

    changed loadhighscore function input variable names

--- js/highscoreparser.js
+++ js/highscoreparser.js
@@ -1,11 +1,11 @@
 // add highscore entries to table
-function loadHighscore(nick, time, score) {
+function loadHighscore(xnick, xtime, xscore) {
 	$.getJSON("commit.php",function(result){
 		$('.score-board table').empty();
 		$('.score-board table').append('<tr><th class="refresh-highscore"></th><th>Nickname</th><th>Time played</th><th>Score</th></tr>');
 		$.each(result, function(i){
 			var n = Number(i) + 1;
-			if(nick == result[i].nick && time == result[i].time && score == result[i].score) {
+			if(xnick == result[i].nick && xtime == result[i].time && xscore == result[i].score) {
 				$('.score-board table').append('<tr class="hilight"><td>' + n + '.</td><td>' + result[i].nick + '</td><td>' + result[i].time + '</td><td>' + result[i].score + '</td></tr>');
 			} else {
 				$('.score-board table').append('<tr><td>' + n + '.</td><td>' + result[i].nick + '</td><td>' + result[i].time + '</td><td>' + result[i].score + '</td></tr>');
