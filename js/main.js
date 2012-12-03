1e77a3b7bbcd0cc40f20670d3c945b737f488604
Roope Merikukka <roope.merikukka@gmail.com>
2012-12-03 16:33:48 +0200

    copied rotation functionality to main.js

--- js/main.js
+++ js/main.js
@@ -178,6 +178,16 @@ $(function () {
             toggleMute(soundArray);
         });
 
+        $(".refresh-image").click(function(){
+            var value = 0;
+            console.log("rotate clicked");
+            value +=720;
+            $(this).rotate({
+                animateTo: value,
+                duration: 2000
+            });
+        });
+
     });
     
 });
--- js/rotation.js
+++ js/rotation.js
@@ -1,5 +1,5 @@
-var value = 0;
 $(".refresh-image").click(function(){
+	var value = 0;
 	console.log("rotate clicked");
 	value +=720;
 	$(this).rotate({
