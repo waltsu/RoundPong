ec59493593f7e05e9fa54c8440da9e1409306792
Roope Merikukka <roope.merikukka@gmail.com>
2012-12-03 16:19:53 +0200

    rewrote rotation function

--- js/rotation.js
+++ js/rotation.js
@@ -1,13 +1,13 @@
 var value = 0;
 $(".refresh-image").rotate({
-	bind: 
+	bind:
 	{
-		click: function(){
+		click: function() {
 			value +=720;
 			$(this).rotate({
-				animateTo:value,
-				duration:2000
+				animateTo: value,
+				duration: 2000
 			});
 		}
 	}
-});​
+});
