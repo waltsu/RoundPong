2bde17e1072ae476ca7e21c84321af738b12700f
Roope Merikukka <roope.merikukka@gmail.com>
2012-12-03 16:26:40 +0200

    rewrote rotation function

--- js/rotation.js
+++ js/rotation.js
@@ -1,13 +1,8 @@
 var value = 0;
-$(".refresh-image").rotate({
-	bind:
-	{
-		click: function() {
-			value +=720;
-			$(this).rotate({
-				animateTo: value,
-				duration: 2000
-			});
-		}
-	}
+$(".refresh-image").click(function(){
+	value +=720;
+	$(this).rotate({
+		animateTo: value,
+		duration: 2000
+	});
 });
