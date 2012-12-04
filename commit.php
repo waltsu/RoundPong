a06c4e9376f3b469dc892635d060ec2d6c4c8815
Roope Merikukka <roope.merikukka@gmail.com>
2012-12-04 11:08:05 +0200

    trying to find the right sql query to get the ranking

--- commit.php
+++ commit.php
@@ -23,7 +23,7 @@ if('POST' == $_SERVER['REQUEST_METHOD']){
 		$sth->bindParam(':id', $id, PDO::PARAM_STR);
 		$sth->execute();
 		$result = $sth->fetch();
-		echo $result[0]+1;
+		echo $result;
 	    $db = null;
 	} catch (PDOException $e) {
 	    print "Error!: " . $e->getMessage() . "<br/>";
